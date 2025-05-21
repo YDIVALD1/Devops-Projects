33d9fb38910ac98f7ce55bcabcf983382b0f4d68
YDIVALD1 <72264681+YDIVALD1@users.noreply.github.com>
2025-05-22 00:47:07 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello from AWS!");
 });
+
 console.log("Updated log message");
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
